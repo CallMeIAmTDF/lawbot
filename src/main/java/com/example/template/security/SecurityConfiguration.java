d434d22bd90b3ecf73c4a9d45d4e8c0050559064
CallMeIAmTDF <131275170+CallMeIAmTDF@users.noreply.github.com>
2025-08-04 23:17:03 +0700

    Initial commit

---
+++ src/main/java/com/example/template/security/SecurityConfiguration.java
@@ -0,0 +1,61 @@
+package com.example.template.security;
+
+import com.example.template.util.constant.PredefinedRole;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfiguration {
+    private static final String[] PUBLIC_ENDPOINTS = {
+            "/auth/**",
+    };
+
+    private final CustomJwtDecoder customJwtDecoder;
+
+    public SecurityConfiguration(CustomJwtDecoder customJwtDecoder) {
+        this.customJwtDecoder = customJwtDecoder;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity,
+                                           CustomAuthenticationEntryPoint customAuthenticationEntryPoint) throws Exception {
+        httpSecurity.authorizeHttpRequests(request -> request.requestMatchers(PUBLIC_ENDPOINTS)
+                .permitAll()
+                .requestMatchers("/users/lock/*").hasAuthority(PredefinedRole.ROLE_ADMIN)
+                .anyRequest().authenticated());
+
+        httpSecurity.oauth2ResourceServer(oauth2 -> oauth2.jwt(jwtConfigurer -> jwtConfigurer
+                        .decoder(customJwtDecoder)
+                        .jwtAuthenticationConverter(jwtAuthenticationConverter()))
+                .authenticationEntryPoint(customAuthenticationEntryPoint));
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        jwtGrantedAuthoritiesConverter.setAuthorityPrefix("");
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+
+        return jwtAuthenticationConverter;
+    }
+}
---
+++ src/main/java/com/example/template/util/constant/PredefinedRole.java
@@ -0,0 +1,7 @@
+package com.example.template.util.constant;
+
+public class PredefinedRole {
+    public static final String ROLE_ADMIN = "ROLE_ADMIN";
+
+    private PredefinedRole() {}
+}
